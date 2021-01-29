c9ac62f62a7be7aaef6b3f1ee39f8d3ffb9326d3
criss02-cs <cristianoaloigi0@gmail.com>
2021-01-29 10:09:46 +0100

    Creato il database e la tabella categoria

--- ProgettoNoleggioAuto/src/progettonoleggioauto/ProgettoNoleggioAuto.java
+++ ProgettoNoleggioAuto/src/progettonoleggioauto/ProgettoNoleggioAuto.java
@@ -1,21 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package progettonoleggioauto;
 
-/**
- *
- * @author crist
- */
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ProgettoNoleggioAuto {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        creaDatabase();
+        creaTabellaCategoria();
+        /*
+        Frame
+        */
     }
     
+    private static void creaDatabase(){
+        String url;
+        url = "jdbc:sqlite:./noleggioauto.db";
+        try (Connection conn = DriverManager.getConnection(url)){
+            if(conn != null){
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("Database creato con successo");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    private static void creaTabellaCategoria(){
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:./noleggioauto.db");
+            System.out.println("Apertura del database avvenuta con successo");
+            stmt = c.createStatement();
+            String creaCategoria = "CREATE TABLE IF NOT EXISTS categorie (" + "\n"
+                    + " id_categoria int auto_increment primary key," + "\n"
+                    + " descrizione varchar(50) NOT NULL," + "\n"
+                    + " noleggioAlGiorno int NOT NULL," + "\n"
+                    + " noleggioAlChilometro int NOT NULL" + "\n"
+                    + " );";
+            stmt.executeUpdate(creaCategoria);
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Tabella creata con successo");
+    }
 }
